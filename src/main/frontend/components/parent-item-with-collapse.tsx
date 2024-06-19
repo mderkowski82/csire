21067280d0746b345ccaceea869b47a0528d07ec
Marek Derkowski <mderkowski@nmg.pl>
2024-06-19 09:32:03 +0200

    Zaktualizowano bibliotekę Vaadin i dodano bibliotekę Lucide do projektu
    Zaktualizowano zależności projektu dla biblioteki "@vaadin/bundles", rozbudowując listę zależności. Dodano również bibliotekę "lucide-react" do projektu. Biblioteka Lucide została użyta do zmiany ikon w komponencie "parent-item-with-collapse".

--- src/main/frontend/components/parent-item-with-collapse.tsx
+++ src/main/frontend/components/parent-item-with-collapse.tsx
@@ -1,22 +1,25 @@
 import {useState} from 'react';
-import {SideNavItem, Icon} from '@vaadin/react-components';
+import {HorizontalLayout, Icon, SideNavItem} from '@vaadin/react-components';
 import ParentMenuItem from "Frontend/generated/pl/npesystem/services/tables/ParentMenuItem";
+import {ArrowDown, ArrowUp, ChevronDown, ChevronUp, LineChart, LucideSeparatorHorizontal, SeparatorHorizontal, Spline} from "lucide-react";
 
-export default function ParentMenuItemWithCollapse({parentMenuItem}: {parentMenuItem: ParentMenuItem}) {
+export default function ParentMenuItemWithCollapse({parentMenuItem}: { parentMenuItem: ParentMenuItem }) {
 	const [isOpen, setIsOpen] = useState(false);
 	const toggleIsOpen = () => setIsOpen(!isOpen);
 
 	return (
-		<>
+		<div className={"w-full border-b"}>
 			<SideNavItem onClick={toggleIsOpen}>
-				{isOpen ? <Icon icon="angle-down" /> : <Icon icon="angle-right" />}
-				{parentMenuItem.label}
+				<HorizontalLayout  className={"flex justify-between items-center"}>
+					{parentMenuItem.label}
+					{isOpen ? <ChevronUp/> : <ChevronDown/>}
+				</HorizontalLayout>
 			</SideNavItem>
 			{isOpen && parentMenuItem.childs?.map(childMenuItem =>
 				<SideNavItem path={`entity/${parentMenuItem.position}/${childMenuItem.position}`} key={childMenuItem.position}>
 					{childMenuItem.label}
 				</SideNavItem>
 			)}
-		</>
+		</div>
 	);
 };
